36f861c4b90f389ce932dd4c52b569f615e3208d
Van Tuan Pham <vantuanpham95@gmail.com>
2018-10-24 09:59:56 +0700

    Add: Some defs

--- vpn.py
+++ vpn.py
@@ -49,9 +49,18 @@ def username_is_exist(conn, username):
 	if data is None:
 		return False
 	else:
-		print("YEP")
+		# print("YEP")
 		return True
  
+def select_hashed_passwd(conn, username):
+	cur = conn.cursor()
+	cur.execute("select password_hash FROM users WHERE username=?", (username,))
+	hassed_passwd = cur.fetchone()[0]
+	return hassed_passwd
+
+def hashing_passwd(password):
+	return(generate_password_hash(password))
+
 def main():
     database = "db.sqlite"
  
@@ -64,10 +73,21 @@ def main():
         # print("2. Query all users")
         # select_all_users(conn)
  
- 		print("3. Check if user is exist")
- 		if username_is_exist(conn, "tuanpv") == True:
- 			print("Yes")
- 		print("No")
- 
+ 		# print("3. Check if user is exist")
+ 		# if username_is_exist(conn, "tuanpv") == True:
+ 		# # print(username_is_exist(conn, "tuanpv"))
+ 		# 	print("YEP")
+ 		# else:
+ 		# 	print("No")
+
+ 		print("4. Select Hashed password")
+ 		if username_is_exist(conn, "tuanpv"):
+ 			print(select_hashed_passwd(conn, "tuanpv"))
+ 		else:
+ 			print("Username not exist!")
+
+ 		print("5. Hasshing inputed passwd")
+ 		print(hashing_passwd("123456"))
+
 if __name__ == '__main__':
     main()
